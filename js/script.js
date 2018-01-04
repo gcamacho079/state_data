681a270392f9f6bb7df3dc5afd04552edd9b9f27
Lupe <lupe@acoorg.com>
2018-01-04 13:07:41 -0800

    Add json index info to map svg paths; create buildDesktopView fxn

--- js/script.js
+++ js/script.js
@@ -4,10 +4,46 @@ var permits = {
     permits.getAllStateData();
   },
 
-  /*createMapFunctionality: function() {
+  getAllStateData: function() {
+    $.ajax("js/stateinfo.json", {
+      dataType: "json",
+      type: "GET",
+      success: function(data) {
+        $("#loadingDiv").hide();
+        permits.createMapFunctionality(data);
+        permits.buildMobileView(data);
+      }
+    });
+  },
+
+  buildMobileView: function(data) {
+    let stateArrayIndex = 0; //For building out element IDs
+    let countyArrayIndex = 0;
+
+    data.states.forEach(function(state) {
+      	$('.info-container-stacked').append("<div class='panel-group' id='panel-group" + stateArrayIndex + "'><div class='panel panel-default state-panel'><a data-toggle='collapse' class='collapsed' aria-expanded='false' href='#state" + stateArrayIndex+ "'>" + state.name + "</a></div></div>");
+      	$('#panel-group' + stateArrayIndex).append("<div class='panel-collapse collapse' id='state" + stateArrayIndex + "' aria-expanded='false'><ul class='list-group' id='list-group-state" + stateArrayIndex + "'></ul></div>");
+		countyArrayIndex = 0;
+
+		state.counties.forEach(function(county) {
+			$("#list-group-state" + stateArrayIndex).append("<li class='list-group-item collapse'><a data-toggle='collapse' class='collapsed' href='#state" + stateArrayIndex + "county" + countyArrayIndex + "'>" + county.name + " County</a><div class='panel-collapse collapse' id='state" + stateArrayIndex + "county" + countyArrayIndex + "' aria-expanded='false'></div></li>");
+
+			county.cities.forEach(function(city) {
+				$("#state" + stateArrayIndex + "county" + countyArrayIndex).append("<a class='city-link' href='" + city.url + "' target='_blank'><i class='fa fa-chevron-right' aria-hidden='true'></i> " + city.name + "</a>")
+
+			});
+			countyArrayIndex++;
+		});
+
+		stateArrayIndex++;
+    });
+  },
+
+  createMapFunctionality: function(data) {
     $("path, circle").click(function(e) {
-      $(".info-container").empty();
-      permits.getStateInfo($(this).attr("id"));
+      $(".info-container-map").empty();
+      let stateIndex = $(this).data('json-index');
+      permits.buildDesktopView(data.states[stateIndex]);
     });
 
     $("path, circle").hover(function(e) {
@@ -24,40 +60,10 @@ var permits = {
       $('#info-box').css('top',e.pageY-$('#info-box').height()-offset.top);
       $('#info-box').css('left',e.pageX-offset.left+($('#info-box').width()/2)-50);
     }).mouseover();
-  },*/
-
-  getAllStateData: function() {
-    $.ajax("js/stateinfo.json", {
-      dataType: "json",
-      type: "GET",
-      success: function(data) {
-        $("#loadingDiv").hide();
-        permits.buildMobileView(data);
-      }
-    });
   },
 
-  buildMobileView: function(data) {
-    let stateArrayIndex = 0; //For building out element IDs
-    let countyArrayIndex = 0;
-
-    data.states.forEach(function(state) {
-      	$('.info-container').append("<div class='panel-group' id='panel-group" + stateArrayIndex + "'><div class='panel panel-default state-panel'><a data-toggle='collapse' class='collapsed' aria-expanded='false' href='#state" + stateArrayIndex+ "'>" + state.name + "</a></div></div>");
-      	$('#panel-group' + stateArrayIndex).append("<div class='panel-collapse collapse' id='state" + stateArrayIndex + "' aria-expanded='false'><ul class='list-group' id='list-group-state" + stateArrayIndex + "'></ul></div>");
-		countyArrayIndex = 0;
-
-		state.counties.forEach(function(county) {
-			$("#list-group-state" + stateArrayIndex).append("<li class='list-group-item collapse'><a data-toggle='collapse' class='collapsed' href='#state" + stateArrayIndex + "county" + countyArrayIndex + "'>" + county.name + " County</a><div class='panel-collapse collapse' id='state" + stateArrayIndex + "county" + countyArrayIndex + "' aria-expanded='false'></div></li>");
-
-			county.cities.forEach(function(city) {
-				$("#state" + stateArrayIndex + "county" + countyArrayIndex).append("<a class='city-link' href='" + city.url + "' target='_blank'><i class='fa fa-chevron-right' aria-hidden='true'></i> " + city.name + "</a>")
-
-			});
-			countyArrayIndex++;
-		});
-
-		stateArrayIndex++;
-    });
+  buildDesktopView: function(data) {
+    console.log(data);
   }
 }
 
